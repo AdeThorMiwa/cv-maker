694b71b8bddb990011ae8a037e1e2d9159e6e3ef
Almighty Tom <tom@TOMs-MacBook-Pro.local>
2023-02-12 22:43:47 +0100

    update backend url

--- src/utils/agent.js
+++ src/utils/agent.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 let BASE_URL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:5000/"
-        : "https://cv-buildr.herokuapp.com/";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/"
+    : "https://cv-builder-api-production.up.railway.app/";
 
 let axiosInstance = axios.create({
     baseURL: BASE_URL,
